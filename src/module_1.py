1bd11eddd6cb212e134f9df9500fb0351de7d542
naorex <nao23r@gmail.com>
2025-09-07 19:14:04 +0900

    UPDATE : 自作関数をモックに置き換え処理 次回からは外部関数のモック置き換えからスタート

--- src/module_1.py
+++ src/module_1.py
@@ -1,4 +1,4 @@
-import sqlite3
+import requests
 
 
 def add_numbers(a: int, b: int) -> int:
@@ -9,6 +9,20 @@ def all_books(con):
     books = con.execute("SELECT * FROM books").fetchall()
     con.close()
     return books
+
+
+# ======================================
+# モックテスト
+# ======================================
+
+
+def get_user(id: str) -> dict:
+    response = requests.get(f"https://example.com/?id={id}")
+    return response.json()
+
+
+def get_user_names(ids: list[str]) -> list[str]:
+    return [get_user(id)["name"] for id in ids]
 
 
 if __name__ == "__main__":
--- tests/test_module_1.py
+++ tests/test_module_1.py
@@ -1,6 +1,13 @@
 import sqlite3
 
-from src.module_1 import add_numbers, all_books
+import pytest
+
+from src import module_1
+from src.module_1 import add_numbers, all_books, get_user_names
+
+# ======================================
+# 単発テスト
+# ======================================
 
 
 def test_add_numbers_1():
@@ -13,9 +20,61 @@ def test_add_numbers_2():
     assert add_numbers(-1, -2) == -3
 
 
+# ======================================
+# fixture 機能を使う
+# ======================================
+
+
 def test_all_books_1():
-    con = sqlite3.connect(":memory:")
+    """使わない場合"""
+    con = sqlite3.connect(":memory:")  # エラーがでるため一時的な接続でテスト
     con.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, author TEXT)")
     books = all_books(con)
     assert books == []
     con.close()
+
+
+@pytest.fixture
+def db_con():
+    con = sqlite3.connect(":memory:")
+    con.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, author TEXT)")
+    return con
+
+
+def test_all_books_1_w_fixture(db_con):
+    """使う場合"""
+    books = all_books(db_con)
+    assert books == []
+    db_con.close()
+
+
+@pytest.fixture
+def db_con_yield():
+    con = sqlite3.connect(":memory:")
+    con.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, author TEXT)")
+    print("before yield")
+    yield con  # -> return の代わりに yield と記述
+    print("after yield")
+    con.close()
+
+
+def test_all_books_1_w_fixture_yield(db_con_yield):
+    """使う場合"""
+    books = all_books(db_con_yield)
+    assert books == []
+
+
+# ======================================
+# monkeypatch 機能を使う
+# ======================================
+
+
+def test_get_user_names_1(monkeypatch):  # 引数に monkeypatch を指定しておく
+    def mock_get_user(id):
+        """モック"""
+        print("start mock_get_user")
+        return {"name": "鈴木"}
+
+    # module_1 の get_user を mock_get_user に差し替える処理
+    monkeypatch.setattr(module_1, "get_user", mock_get_user)
+    assert get_user_names(["A001", "A002"]) == ["鈴木", "鈴木"]
